2b9d62f11f64c27538ce1d1bfa0c80e214448aa5
Noamir <noamir2512@gmail.com>
2020-07-13 20:08:09 +0300

    noa-amir-project

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 #add fix to exercise3 here
+sed -i ':a;N;$!ba; s/deny/Allow/2' /etc/apache2/sites-available/default
+sudo service apache2 restart
--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+
+echo "Host 192.168.100.11, server2
+    StrictHostKeyChecking no" >> /home/vagrant/.ssh/config
+
+su vagrant -c "ssh-keygen -f /home/vagrant/.ssh/id_rsa -N ''"
+
+#sudo apt-get install sshpass
+
+#sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub vagrant@192.168.100.11
